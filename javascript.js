7274c9e224f8472178871db8f45c70d65b701a3d
gldsct <188839581+gldsct@users.noreply.github.com>
2025-04-17 15:41:43 +0545

    Make UI buttons interactive
    Make the UI buttons interactive, allowing users to use the calculator by
    clicking on buttons on the page.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 function add(firstNum, secondNum) {
-    return firstNum + secondNum;
+    return +firstNum + +secondNum;
 }
 
 function subtract(firstNum, secondNum) {
-    return firstNum - secondNum;
+    return +firstNum - +secondNum;
 }
 
 function multiply(firstNum, secondNum) {
-    return firstNum * secondNum;
+    return +firstNum * +secondNum;
 }
 
 function divide(firstNum, secondNum) {
@@ -15,7 +15,54 @@ function divide(firstNum, secondNum) {
         return "Error";
     }
     else {
-        return firstNum / secondNum;
+        return +firstNum / +secondNum;
     }
 }
 
+results = document.querySelector(".results");
+display = document.querySelector(".display");
+buttons = document.querySelectorAll("button");
+
+numericButtons = document.querySelectorAll(".numbers");
+operatorButtons = document.querySelectorAll(".operator");
+equalsButton = document.querySelector("#equals");
+clearButton = document.querySelector("#clear");
+
+numericButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        display.textContent += `${event.target.textContent} `;
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        display.textContent += `${event.target.textContent} `;
+    });
+});
+
+equalsButton.addEventListener("click", (event) => {
+    let expression = display.textContent;
+    let expressionArray = expression.split(" ");
+    switch (expressionArray[1]) {
+        case "+":
+            expressionCalc = add(expressionArray[0], expressionArray[2]);
+            break;
+        case "-":
+            expressionCalc = subtract(expressionArray[0], expressionArray[2]);
+            break;
+        case "x":
+            expressionCalc = multiply(expressionArray[0], expressionArray[2]);
+            break;
+        case "/":
+            expressionCalc = divide(expressionArray[0], expressionArray[2]);
+            break;
+        default:
+            break;
+    }
+    results.textContent = `${expressionCalc}`;
+});
+
+clearButton.addEventListener("click", (event) => {
+    display.textContent = "";
+    results.textContent = "0";
+});
